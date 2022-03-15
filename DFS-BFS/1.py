264266dd27ede8c32f3f1a2492625431a9f90104
kiyeol <kyday63@gmail.com>
2022-03-15 22:50:00 +0900

    dfs

---
+++ DFS-BFS/1.py
@@ -0,0 +1,30 @@
+# 탐색 ? 많은 양의 데이터 중에서 원하는 데이터를 찾는 과정
+# 대표적으로 DFS / BFS 그래프 탐색 알고리즘이 있다.
+# DFS 깊이 우선 탐색
+
+
+
+graph = [
+  [],
+  [2,3,8],
+  [1,7],
+  [1,4,5],
+  [3,5],
+  [3,4],
+  [7],
+  [2,6,8],
+  [1,7]
+]
+
+visited = [False] * 9
+
+def dfs(graph, v, visited):
+  visited[v] = True
+  print(v, end=' ')
+
+  for i in graph[v]:
+    if not visited[i]:
+      dfs(graph, i , visited)
+
+      
+dfs(graph,1,visited)

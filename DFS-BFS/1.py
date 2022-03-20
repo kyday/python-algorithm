fd9a5059834e5ee9dd2e1f66a0da487c97c3b3b3
kiyeol <kyday63@gmail.com>
2022-03-20 23:41:53 +0900

    삽입정렬

--- DFS-BFS/1.py
+++ DFS-BFS/1.py
@@ -16,12 +16,15 @@ graph = [
   [1,7]
 ]
 
+# 각 노드가 방문된 정보들은 모든 노드를 방문하지 않은 것처럼 처리
 visited = [False] * 9
 
 def dfs(graph, v, visited):
+  # 현재 노드 방문 처리
   visited[v] = True
   print(v, end=' ')
 
+# 방문하지 않은 1차원 배열에서 다른 노드를 재귀적으로 방문
   for i in graph[v]:
     if not visited[i]:
       dfs(graph, i , visited)
